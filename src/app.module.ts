d37065f0978437a583043526ac2a57dca66e4325
yansobol <yanchoper@gmail.com>
2024-03-28 05:07:33 +0200

    ADD: chatgpt and telegram modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ChatgptModule } from './chatgpt/chatgpt.module';
+import { TelegramModule } from './telegram/telegram.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ChatgptModule, TelegramModule],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
